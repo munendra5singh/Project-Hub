93080fae3f88a914df81efa1cf258c00242a9a6d
Munendra singh <singh2005munendra@gmail.com>
2026-08-20 13:01:17 +0530

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -161,7 +161,6 @@ const projectsData = [
     image: "https://images.unsplash.com/photo-1580519542036-c47de6196ba5?w=600&auto=format&fit=crop&q=80",
     liveUrl: "projects/p-12-excel/money_breck_calculater.xlsx"
   },
-
   {
   id: 19,
   name: "Morse Code Learning",
@@ -171,4 +170,13 @@ const projectsData = [
   image: "https://www.nationalworld.com/jpim-static/image/2023/04/26/15/NWLD-composite-morsecode-km.jpg?trim=0,0,0,0&crop=&width=640&quality=65",
   liveUrl: "projects/p-19-morse-code/index.html"
 },
+ {
+  id: 20,
+  name: "Waveform Studio",
+  description: "Modern audio visualizer, waveform generator & sound editor web app.",
+  category: "Audio & Tools",
+  technologies: ["HTML", "CSS", "JavaScript", "Web Audio API"],
+  image: "https://images.unsplash.com/photo-1598488035139-bdbb2231ce04?w=600&auto=format&fit=crop&q=80",
+  liveUrl: "projects/p-20-WaveformStudio/index.html"
+}
 ];
